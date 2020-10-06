a93d1e1987a6474b1e839c2805f88c0f478bd332
Thiago Nogueira <thiagonogueira.if@gmail.com>
2020-10-06 11:01:08 -0300

    faz um gráfico histograma por tipo de ocorrência

--- limpeza.R
+++ limpeza.R
@@ -13,17 +13,14 @@ library(plyr)
 
 # Carrega o dataset em .csv
 
-sol2018 <- readr::read_csv("ds2018.csv")
-str(sol2018)
-
-sol1418 <- readr::read_csv("solicitações14a18.csv")
+sol1418 <- read.csv("solicitações14a18.csv")
 str(sol1418)
 
  #Exclui as colunas sujas do dataframe
-names(sol2018)[7] <- ("n_casa")
-sol2018 <- sol2018[ -c(14:27)]
-str(sol2018)
-sol2018
+#names(sol2018)[7]  <- ("n_casa")
+#sol2018 <- sol2018[ -c(14:27)]
+#str(sol2018)
+#sol2018
 
 #Gera a time series a partir da contagem de amostras 
 
@@ -51,7 +48,7 @@ hist(ts1418$obs, breaks = 500)
 
 vam <- count(ts1418, 'obs')
 vam
-
+str(vam)
 
 p2 <- ggplot(vam, aes(x = obs, y = freq)) + geom_point() +
   scale_x_log10(breaks = trans_breaks("log10", function(x) 10^x),
@@ -62,13 +59,13 @@ p2 <- ggplot(vam, aes(x = obs, y = freq)) + geom_point() +
 p2 + annotation_logticks(sides="trbl")  
 
 ###########################################################################
-s2014 = filter(sol1418, year(`Dt Abertura`) == 2014)
+s2014 = filter(sol1418, year(Dt.Abertura) == 2014)
 
 series = list()
 for(i in 1:5){
-  temp = filter(sol1418, year(`Dt Abertura`) == (2013 +i))
+  temp = filter(sol1418, year(Dt.Abertura) == (2013 +i))
   print(temp)
-  series[[i]] <- as.data.frame(count(temp$`Dt Abertura`)) %>% 
+  series[[i]] <- as.data.frame(count(temp$Dt.Abertura)) %>% 
                 complete(Date =seq(ymd(paste(as.character(2013+i), "-01-01", sep="")), ymd(paste(as.character(2013+i), "-12-31", sep="")), by = "1 day"))
   
 }
--- time_series.R
+++ time_series.R
@@ -1,16 +1,40 @@
+require(ggplot2)
+library(ggplot2)
+library(tidyr)
+library(tibble)
+library(dplyr)
+library(lubridate)
+library(plyr)
+
+
+
 # Carrega o dataset em .csv
 sol1418 <- read.csv("solicitações14a18.csv")
 sol1418$Dt.Abertura <- as_date(sol1418$Dt.Abertura)
 
-
+# Facet by two variables: vs and am.
+# Rows are vs and columns are am
+p3 <-  ggplot(data = sol1418, aes(Prefeitura.Bairro, Tipo.de.Ocorrência)) + geom_histogram()
+    p3 + facet_grid( Prefeitura.Bairro~ Tipo.de.Ocorrência)
+p3
 #Filtra o dataset por ano
-s2014 = filter(sol1418, year(Dt.Abertura) == 2014)
+s2014 = filter(sol1418, year(Dt.Abertura) == 2018)
 #Converte  as solicitações em uma série temporal de solicitações por dia
 ts2014 <- as.data.frame(count(s2014$Dt.Abertura))
 #completa a série com as datas faltantes
 ts2014 <- ts2014 %>% 
-  complete(x = seq(ymd("2014-01-01"), ymd("2014-12-31"), by = "1 day")) 
+  complete(x = seq(ymd("2018-01-01"), ymd("2018-12-31"), by = "1 day")) 
   ts2014[is.na(ts2014)] <- 0
                               
-hist(ts2014$freq, breaks = 500)                
-  
+cont <- count(ts2014$freq)
+names(cont)[1] <- ('obs')
+p2 <- ggplot(cont, aes(x = obs, y = freq)) + geom_point() +
+  scale_x_log10(breaks = trans_breaks("log10", function(x) 10^x),
+                labels = trans_format("log10", math_format(10^.x))) +
+  scale_y_log10(breaks = trans_breaks("log10", function(x) 10^x),
+                labels = trans_format("log10", math_format(10^.x))) +
+  theme_bw() 
+p2 + annotation_logticks(sides="trbl")  
+
+
+
