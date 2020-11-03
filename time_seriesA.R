72982f0f274508d507e9c75574c75a378193096e
Thiago Felipe Nogueira <thiago.nogueira@defesacivil.pms.ad>
2020-11-03 17:13:41 -0300

    formata e exporta arquivo tsv que será entrada para o HistLogFiles

--- time_seriesA.R
+++ time_seriesA.R
@@ -16,8 +16,10 @@ sgdc <- sgdc %>%  separate(data_solic,
                            sep = " ",
                            remove = FALSE)
 
+unique(sgdc$ocorr_solic)
+
 #conmta as observações pela data e organiza num dataframe
-serie_temp <- as.data.frame(count(sgdc[sgdc$ocorr_solic == "AMEACA DE DESLIZAMENTO",], "data_proc"))
+serie_temp <- as.data.frame(count(sgdc[sgdc$ocorr_solic == "DESLIZAMENTO DE TERRA" & sgdc$ocorr_vist == "DESLIZAMENTO DE TERRA",], "data_proc"))
 
 #converte data_proc de char para date
 serie_temp$data_proc <- as_date(serie_temp$data_proc)
@@ -28,5 +30,22 @@ serie_temp <- serie_temp %>%
               complete(data_proc = seq(ymd("2004-07-01"), ymd("2019-12-31"), by = "1 day"))
 serie_temp[is.na(serie_temp)] <- 0
 
-filter(serie_temp, year(data_proc) >= 2010 & year(data_proc) <= 2019 ) %>%  ggplot(aes(x = data_proc, y = freq)) + geom_line()
- 
+filter(serie_temp, data_proc >= ymd("2005-01-01") & data_proc <= ymd("2019-12-31") ) %>%  ggplot(aes(x = data_proc, y = freq)) + geom_line()
+
+#Cria a série temporal que será exportada
+serie_export <- filter(serie_temp, data_proc >= ymd("2005-01-01") & data_proc <= ymd("2019-12-31"))
+
+#Substitui as datas pela sequência de números
+serie_export <- serie_export  %>%  mutate(dias = seq(1, nrow(serie_export)))
+
+#Reordena as colunas do dataset 
+serie_export <- serie_export[, c(3,2,1)]
+
+#Remove a coluna de datas
+serie_export <- subset(serie_export, select = -c(data_proc))
+
+serie_export  %>%  ggplot(aes(x = dias, y = freq )) +geom_point()
+
+#exporta o arquivo .tsv
+write.table(serie_export  , file='socDDT.tsv', quote=FALSE, sep='\t', row.names = FALSE) 
+
